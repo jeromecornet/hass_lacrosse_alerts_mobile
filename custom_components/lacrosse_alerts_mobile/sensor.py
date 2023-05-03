4046ed5165363f0d285eb5e62e0962577c955957
Jérôme Cornet <jerome@jeromecornet.com>
2023-05-03 11:28:03 -0400

    Use Celsius

--- custom_components/lacrosse_alerts_mobile/sensor.py
+++ custom_components/lacrosse_alerts_mobile/sensor.py
@@ -3,7 +3,7 @@ import logging
 import voluptuous as vol
 
 from homeassistant.components.sensor import ENTITY_ID_FORMAT, PLATFORM_SCHEMA
-from homeassistant.const import TEMP_FAHRENHEIT, CONF_ID
+from homeassistant.const import TEMP_CELSIUS, CONF_ID
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 
@@ -19,66 +19,78 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the lacrosse alerts mobile platform."""
     import pylacrossapi
     device_id = config.get(CONF_ID)
-    unit_measure = 0
-    time_zone = 10
+    unit_measure = 1
+    time_zone = 17
     lacrosse_device = pylacrossapi.lacrosse(device_id, unit_measure, time_zone)
-    add_devices([LaCrosseAmbientSensor(lacrosse_device), LaCrosseProbeSensor(lacrosse_device), LaCrosseHumidSensor(lacrosse_device)])
+    add_devices([LaCrosseAmbientSensor(lacrosse_device)])
+    add_devices([LaCrosseProbeSensor(lacrosse_device)])
+    add_devices([LaCrosseHumidSensor(lacrosse_device)])
 
-class LaCrosseAlertsMobileSensor(Entity):
+
+class LaCrosseAmbientSensor(Entity):
     """Representation of a LaCrosse Alerts Mobile Sensor."""
     def __init__(self, lacrosse_device):
         """Initialize the sensor."""
         self._lacrosse_device = lacrosse_device
         self._state = None
 
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Lacrosse.Ambient_Temp'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return TEMP_CELSIUS
+        
     def update(self):
         obs = self._lacrosse_device.getObservation(1)
-        self._ambient = obs[0]['ambient_temp']
-        self._probe = obs[0]['probe_temp']
-        self._humidity = obs[0]['humidity']
+        self._state = obs[0]['ambient_temp']
+        
+class LaCrosseProbeSensor(Entity):
+    """Representation of a LaCrosse Alerts Mobile Sensor."""
+    def __init__(self, lacrosse_device):
+        """Initialize the sensor."""
+        self._lacrosse_device = lacrosse_device
+        self._state = None
 
-class LaCrosseAmbientSensor(LaCrosseAlertsMobileSensor):
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Outside Ambient'
+        return 'Lacrosse.Probe_Temp'
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        return self._ambient
+        return self._state
 
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement."""
-        return TEMP_FAHRENHEIT
+        return TEMP_CELSIUS
+        
+    def update(self):
+        obs = self._lacrosse_device.getObservation(1)
+        self._state = obs[0]['probe_temp']
 
-class LaCrosseProbeSensor(LaCrosseAlertsMobileSensor):
+
+class LaCrosseHumidSensor(Entity):
+    """Representation of a LaCrosse Alerts Mobile Sensor."""
+    def __init__(self, lacrosse_device):
+        """Initialize the sensor."""
+        self._lacrosse_device = lacrosse_device
+        self._state = None
+
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Outside Probe'
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._probe
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return TEMP_FAHRENHEIT
-
-class LaCrosseHumidSensor(LaCrosseAlertsMobileSensor):
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return 'Outside Humidity'
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._humidity
+        return 'Lacrosse.Ambient_Humidity'
 
     @property
     def unit_of_measurement(self):
@@ -89,3 +101,12 @@ class LaCrosseHumidSensor(LaCrosseAlertsMobileSensor):
     def icon(self):
         """Icon to use in the frontend."""
         return 'mdi:water-percent'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def update(self):
+        obs = self._lacrosse_device.getObservation(1)
+        self._state = obs[0]['humidity']
